a36ae826a9620a14272c5be120fb79acb921f06a
jilakarajagadeesh7 <138190783+jilakarajagadeesh7@users.noreply.github.com>
2023-07-06 10:01:08 +0530

    Update sub.h

--- sub.h
+++ sub.h
@@ -1,3 +1,6 @@
+//This code represents a header file sub.h that declares several function prototypes related to banking operations, 
+//including cashDeposit(), cashWithdrawal(), fundTransfer(), accountServices(), balanceInquiry(), changePIN(), and statementRequest().
+
 #ifndef SUB_H
 #define SUB_H
 
