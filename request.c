0cc6c3c8b09f526fe972a08a3503b751725dbad1
jilakarajagadeesh7 <138190783+jilakarajagadeesh7@users.noreply.github.com>
2023-07-18 22:46:05 +0530

    Update request.c

--- request.c
+++ request.c
@@ -1,14 +1,16 @@
-//This code defines a function statementRequest() that retrieves and displays the transaction statement from a file named "transactions.txt". 
-//It first attempts to open the file and checks for any errors. If the file is successfully opened, 
-//it reads each line of the file and prints it as part of the statement. Finally, it closes the file.
-
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-void statementRequest() {
+#define MAX_TRANSACTIONS 5
+
+void statementRequest(const char* userName) {
     FILE *file;
     char transaction[1000];
+    char userTransaction[MAX_TRANSACTIONS][1000];
+    int transactionCount = 0;
+    int totalTransactionCount = 0;
 
     // Open the transaction file
     file = fopen("transactions.txt", "r");
@@ -17,11 +19,40 @@ void statementRequest() {
         return;
     }
 
-    // Print each transaction from the file
-    printf("Statement:\n");
+    // Count the total number of transactions for the user
     while (fgets(transaction, sizeof(transaction), file) != NULL) {
-        printf("%s", transaction);
+        if (strstr(transaction, userName) != NULL) {
+            totalTransactionCount++;
+        }
+    }
+
+    // Calculate the number of transactions to display
+    int numTransactions = (totalTransactionCount > MAX_TRANSACTIONS) ? MAX_TRANSACTIONS : totalTransactionCount;
+    int start = totalTransactionCount - numTransactions;
+    int currentTransaction = 0;
+
+    // Rewind the file
+    rewind(file);
+
+    // Find and store the last 5 transactions for the user
+    while (fgets(transaction, sizeof(transaction), file) != NULL) {
+        if (strstr(transaction, userName) != NULL) {
+            if (currentTransaction >= start) {
+                strcpy(userTransaction[currentTransaction % MAX_TRANSACTIONS], transaction);
+            }
+            currentTransaction++;
+        }
     }
 
     fclose(file);
+
+    // Print the last 5 transactions for the user
+    printf("Statement for user '%s':\n", userName);
+    if (numTransactions == 0) {
+        printf("No transactions found for user '%s'.\n", userName);
+    } else {
+        for (int i = 0; i < numTransactions; i++) {
+            printf("%s", userTransaction[i]);
+        }
+    }
 }
