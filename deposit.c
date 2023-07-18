62e7494c64d487589d3e2d50edfcd64e8daf9134
jilakarajagadeesh7 <138190783+jilakarajagadeesh7@users.noreply.github.com>
2023-07-18 22:44:12 +0530

    Update deposit.c

--- deposit.c
+++ deposit.c
@@ -1,28 +1,23 @@
-//This code defines a function cashDeposit() that handles the process of cash deposit in an ATM system.
-//It prompts the user to enter an amount to deposit. If the amount is valid (between 100 and 100,000), it performs the deposit,
-//updates the account balance (not implemented in the given code), and records the transaction in a file called "transactions.txt". 
-//Finally, it displays a success message.
-
 #include <stdio.h>
-#include <stdlib.h>
+#include "sub.h"
 
-void cashDeposit() {
+void cashDeposit(int *balance) {
     double amount;
     FILE *file;
-    
-    printf("valiadate notes:1.100 2.200 3.500 4.2000\n");
+
+    printf("validate notes: 1.100 2.200 3.500 4.2000\n");
 
     // Prompt for deposit amount
     printf("Enter the amount to deposit: ");
     scanf("%lf", &amount);
     // Validate the amount (should be between 100 and 100000)
     if (amount < 100 || amount > 100000) {
-        printf("Invalid amount. Deposit failed.\n");
+        printf("Invalid amount. Amount should be between 100 and 100000. Deposit failed.\n");
         return;
     }
 
     // Perform the deposit and update account balance
-    // Add your logic here
+    *balance += (int)amount;
 
     // Save the transaction to a file
     file = fopen("transactions.txt", "a");
@@ -35,4 +30,7 @@ void cashDeposit() {
     fclose(file);
 
     printf("Deposit successful. Thank you!\n");
+
+    // Display the updated account balance
+    printf("Your updated account balance is: Rs%d\n", *balance);
 }
